89db44758524ff5e6f019bd58eb16a65b1174e9e
ketig <41895005+ketig@users.noreply.github.com>
2025-07-12 23:40:13 +0400

    ....

--- ConsoleApp_StepIND_FirstLab/Program.cs
+++ ConsoleApp_StepIND_FirstLab/Program.cs
@@ -1,9 +1,26 @@
-﻿for (int i = 0; i < 4; i++)
-{
-    Console.WriteLine(i);
-}
+﻿bool valid = IsValidEmail("user@gmail.com");
 
-if (true)
+static bool IsValidEmail(string email)
 {
+    if (string.IsNullOrEmpty(email))
+    {
+        return false;
+    }
 
+    int index = email.IndexOf("@");
+    if (index < 0 /*!email.Contains('@')*/)
+    {
+        return false;
+    }
+
+    // @ -მდე უნდა იყოს მინიმუმ 1 სიმბოლო
+    if (index == 0)
+    {
+        return false;
+    }
+
+    // @ -ის შემდეგ უნდა იყოს მინიმუმ 1 სიმბოლო
+    // უნდა შეიცავდეს წერტილს @ -ის შემდეგ
+
+    return true;
 }
