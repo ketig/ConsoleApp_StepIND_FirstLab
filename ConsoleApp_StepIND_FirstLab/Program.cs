5e5259e441fd8169b244e909ec520330b29b1a82
ketig <41895005+ketig@users.noreply.github.com>
2025-08-06 13:54:43 +0400

    Generics

--- ConsoleApp_StepIND_FirstLab/Program.cs
+++ ConsoleApp_StepIND_FirstLab/Program.cs
@@ -1,92 +1,85 @@
-﻿try
-{
-    int[] Masivi = { 1, 2, 3, 4, 5 };
-    GetElement(Masivi, 5);
-}
-catch (Exception e)
-{
-    Console.WriteLine($"Invalid data: {e.Message}");
-}
+﻿using ConsoleApp_StepIND_FirstLab.Models;
+using System.Collections;
 
-static int GetElement(int[] array, int index)
+Dictionary<string, double> books = new Dictionary<string, double>();
+books.Add("Samanishvilis Dedinacvali", 25.00);
+books.Add("Dedaena", 15.20);
+books.Add("Didostatis Marjvena", 30.00);
+foreach (var book in books)
 {
-    //try
-    //{
-    //    return array[index];
-    //}
-    //catch (IndexOutOfRangeException e)
-    //{
-    //    Console.WriteLine($"Index out of range: {e.Message}");
-    //    throw; // rethrow the exception to be handled by the caller
-    //}
-    //catch (Exception e)
-    //{
-    //    Console.WriteLine($"An error occurred: {e.Message}");
-    //    throw; // rethrow the exception to be handled by the caller
-    //}
-
-
-    if (index > 0 && index < array.Length)
-    {
-        return index;
-    }
-    else
-    {
-        throw new ArgumentOutOfRangeException();
-    }
+    Console.WriteLine($"{book.Key} : {book.Value}");
 }
 
-//try
-//{
-//    Student student = new Student(1, "John", DateTime.Now, "user@gmail.com", 100, "smth");
+Console.WriteLine("===============");
 
-//    student.Grade = 110;
-//}
-//catch (ArgumentOutOfRangeException e)
-//{
-//    Console.WriteLine($"Argument out of range: {e.ParamName}");
-//}
-//catch (Exception e)
-//{
-//    Console.WriteLine(e.Message);
-//}
-//finally
-//{
-//    Console.WriteLine("Finally block executed.");
-//}
+Dictionary<string, int> scores = new Dictionary<string, int>();
+scores["Alice"] = 90;
+scores["Bob"] = 85;
+Console.WriteLine(scores["Alice"]); // 90
 
-//try
-//{
-//    int x = 4;
-//    int y = 1;
+Console.WriteLine(scores["Alice"]);
 
-//    Console.WriteLine(x / y);
-//}
-//catch (DivideByZeroException e)
-//{
-//    Console.WriteLine(e.Message);
-//}
-//finally
-//{
-//    Console.WriteLine("Finally block executed after division.");
-//}
+foreach (var item in scores)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
 
+#region Collections
+ArrayList arrayList = new ArrayList();
+arrayList.Add(1);
+arrayList.Add("Hello");
+arrayList.Add(new Rectangle());
+int a = arrayList.Count;
+List<int> list = new List<int>();
 
+Stack stack = new Stack();
+stack.Push(1);
+stack.Push("Hello");
+stack.Push(new Rectangle());
+object obj = stack.Peek();
+obj = stack.Pop();
 
+Queue queue = new Queue();
+queue.Enqueue(1);
+queue.Enqueue("Hello");
+queue.Enqueue(new Rectangle());
+obj = queue.Peek();
+obj = queue.Dequeue();
 
-//using (MyDisposableClass disposable = new MyDisposableClass())
-//{
-//    Console.WriteLine("using block");
+Hashtable hashtable = new Hashtable();
+hashtable.Add("key1", 1);
+hashtable.Add("key2", 1);
+hashtable.Add("key3", 1);
 
+Dictionary<char, double> keyValuePairs = new Dictionary<char, double>();
+keyValuePairs.Add('a', 1.0);
+keyValuePairs.Add('b', 2.0);
+keyValuePairs.Add('c', 3.0);
+#endregion
 
+#region Generics
+int x = 5;
+int y = 10;
+Swap(ref x, ref y);
 
-//}
+string str = "";
+string str2 = "Hello";
+Swap(ref str, ref str2);
 
-//// IDisposable interface
-//class MyDisposableClass : IDisposable
-//{
-//    public void Dispose()
-//    {
-//        Console.WriteLine("dispose method");
-//    }
-//}
+Rectangle rectangle = new Rectangle(3, 4);
+
+Box<Rectangle> box = new Box<Rectangle>();
+box.Value = rectangle;
+
+//Box<int> box1 = new Box<int>(); // This line will cause a compile-time error because int is not a class type.
+
+static void Swap<T>(ref T a, ref T b) /*where T : class*/
+{
+    (a, b) = (b, a);
+}
+
+public class Box<T> where T : class
+{
+    public T Value { get; set; }
+}
+#endregion
