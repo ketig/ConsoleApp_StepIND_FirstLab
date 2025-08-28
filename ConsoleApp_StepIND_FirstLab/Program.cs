16d730c9fbbd37ae5b61e49fc545d4c37fa5085b
ketig <41895005+ketig@users.noreply.github.com>
2025-08-28 10:24:59 +0400

    age

--- ConsoleApp_StepIND_FirstLab/Program.cs
+++ ConsoleApp_StepIND_FirstLab/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine();
+﻿var birthDate = new DateTime(2000, 09, 2);
+
+int age = GetAge(birthDate);
+Console.WriteLine(age);
+
+
+static int GetAge(DateTime birthDate)
+{
+    var today = DateTime.Now;
+    int age = today.Year - birthDate.Year;
+    return today.DayOfYear > birthDate.DayOfYear ? age : (age - 1);
+}
