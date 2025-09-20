7f31680b2859617a0af6477a5203978e43133a17
ketig <41895005+ketig@users.noreply.github.com>
2025-09-20 20:22:09 +0400

    Reflection

---
+++ ConsoleApp_StepIND_FirstLab/Attributes/Custom.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp_StepIND_FirstLab.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    internal class Custom : Attribute
+    {
+        private int _score;
+
+        public Custom() { }
+
+        public Custom(int score)
+        {
+            _score = score;
+        }
+    }
+
+}
---
+++ ConsoleApp_StepIND_FirstLab/Attributes/MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp_StepIND_FirstLab.Attributes
+{
+    internal class MyClass
+    {
+        [JsonIgnore]
+        [Custom]
+        [Custom(1)]
+        public required string Name { get; set; }
+
+        [JsonIgnore]
+        public int Grade { get; set; }
+
+        [JsonPropertyName("student-score")]
+        [JsonPropertyOrder(2)]
+        [AllowedValues(0, 100)]
+        [Range(0, 100)]
+        public int StudentScore { get; set; }
+
+        [JsonPropertyName("mail")]
+        [JsonPropertyOrder(1)]
+        public required string Mail { get; set; }
+
+        public static void Print()
+        {
+            Console.WriteLine();
+        }
+
+        public static void Test()
+        {
+            MyClass myClass = new MyClass()
+            {
+                Name = "John Doe",
+                Mail = "john@gmail.com",
+            };
+
+            CheckAttributes();
+
+            static void CheckAttributes()
+            {
+                Custom custom = (Custom)Attribute.GetCustomAttribute(typeof(MyClass), typeof(Custom));
+
+                if (custom != null)
+                {
+                    Console.WriteLine(custom.ToString());
+                }
+            }
+        }
+    }
+}
