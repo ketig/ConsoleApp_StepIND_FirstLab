01a53e78a0c40149322e3be8793bf0af8d109bf3
ketig <41895005+ketig@users.noreply.github.com>
2025-09-22 11:58:10 +0400

    Reflection

---
+++ ConsoleApp_StepIND_FirstLab/Attributes/ReflectionClass.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp_StepIND_FirstLab.Attributes
+{
+    internal class ReflectionClass
+    {
+        public const string test = "Reflection";
+
+        public void Run()
+        {
+            Console.WriteLine(test);
+        }
+
+        public static void Test()
+        {
+            ReflectionClass obj = new ReflectionClass();
+
+            Type type = obj.GetType();
+            var method = type.GetMethod("Run");
+
+            method?.Invoke(obj, null);
+        }
+    }
+}
--- ConsoleApp_StepIND_FirstLab/Program.cs
+++ ConsoleApp_StepIND_FirstLab/Program.cs
@@ -1,5 +1,25 @@
-﻿// Github
-// Warning
+﻿// Github ??
 // File
+// Comments
 
+Console.WriteLine();
 
+class Example
+{
+    private static List<string> words = new List<string>();
+
+    /// <summary>
+    /// Open file and read all lines
+    /// </summary>
+    static Example()
+    {
+        using (StreamReader sr = new StreamReader("words.txt"))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                words.Add(line);
+            }
+        }
+    }
+}
